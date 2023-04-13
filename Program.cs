b6384d76f9f04c54e11883adf9e29f340980dc94
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-12 21:01:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lsita1ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double Fahrenheit;
+
+            Console.WriteLine("Conversor de celsius para Fahrenheit");
+            Console.WriteLine();
+            Console.WriteLine("Digite o valor em graus Celsius");
+
+            celsius = double.Parse(Console.ReadLine());
+
+
+            Fahrenheit = (celsius * 9) / (5) + 32;
+            Console.WriteLine("temepratura em F° " + Fahrenheit);
+        }
+    }
+}
